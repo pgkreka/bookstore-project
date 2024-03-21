8bdc64d25073beb9fa12a274a7f0252c340844b7
pgkreka <pelagia.gkreka@gmail.com>
2024-03-21 16:26:20 +0200

    add search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TopBarComponent } from "./top-bar/top-bar.component";
+import { SearchPageComponent } from "./search-page/search-page.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, MatSlideToggleModule, TopBarComponent]
+    imports: [RouterOutlet, MatSlideToggleModule, TopBarComponent, SearchPageComponent]
 })
 export class AppComponent {
   title = 'bookstore-project';
