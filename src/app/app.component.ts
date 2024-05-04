717cffca275a9fb79cf3250d61a6f9962468aa31
Pelagia Gkreka <pelagia.gkreka@gmail.com>
2024-05-04 21:04:52 +0300

    delete component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TopBarComponent } from "./top-bar/top-bar.component";
-import { SearchPageComponent } from "./search-page/search-page.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, MatSlideToggleModule, TopBarComponent, SearchPageComponent]
+    imports: [RouterOutlet, MatSlideToggleModule, TopBarComponent]
 })
 export class AppComponent {
   title = 'bookstore-project';
